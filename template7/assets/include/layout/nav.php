9510eefeae242e2550029ca57dab79c54cfbc350
ayana06 <93752065+ayana06@users.noreply.github.com>
2022-07-06 11:52:57 +0600

    Add files via upload

---
+++ template7/assets/include/layout/nav.php
@@ -0,0 +1,89 @@
+<div class="blockWomens">
+      <div class="blockWomens-title">
+      <h1>WOMEN’S FASHION</h1>
+      <p>Shop our new arrivals from established brands</p>
+    </div>
+      <div class="blockWomens-cards__col">
+        <div class="blockWomens-cards__row">
+          <img src="./assets/img/8 (1).png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockWomens-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+        <div class="blockWomens-cards__row">
+          <img src="./assets/img/9.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockWomens-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+        <div class="blockWomens-cards__row">
+          <img src="./assets/img/6.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockWomens-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+        <div class="blockWomens-cards__row">
+          <img src="./assets/img/7.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockWomens-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="blockMans">
+      <div class="blockMans-title">
+        <h1>MEN’S FASHION</h1>
+        <p>Shop our new arrivals from established brands</p>
+      </div>
+      <div class="blockMans-cards__col">
+       
+        <div class="blockMans-cards__row">
+          <img src="./assets/img/13.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockMans-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+        <div class="blockMans-cards__row">
+          <img src="./assets/img/11.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockMans-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+        <div class="blockMans-cards__row">
+          <img src="./assets/img/10.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockMans-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+        <div class="blockMans-cards__row">
+          <img src="./assets/img/12.png" alt="">
+          <h2>IGURE</h2>
+          <h1>GREEN MUSCLE FIT POLO SHIRT</h1>
+          <div class="blockMans-price">
+            <p>$229.00</p>
+            <span>$129.00</span>
+          </div>
+        </div>
+      </div>
+    </div>
